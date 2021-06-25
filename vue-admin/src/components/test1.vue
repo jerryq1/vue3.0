e24644fd7d59c3af8186935992871b793841789b
jerry <xuzhanhong@gialen.com>
2021-06-25 11:16:50 +0800

    新增gz压缩

--- vue-admin/src/components/test1.vue
+++ vue-admin/src/components/test1.vue
@@ -1,26 +1,25 @@
 <template>
-  <div>
-    我是生产页
-    <Test :bbc.sync="aaaa"></Test>
+  <div @click="changeParent">
+    我是测试组件{{bbc}}
   </div>
 </template>
 
 <script>
-  import Test from 'components/test1.vue'
   export default {
-    name:'product',
+    name:'test',
+
     components: {
       // 子组件引入
-      Test
     },
     props: {
       // 供父组件使用属性
+      bbc:{
+        type:Number,
+      }
     },
     data() {
       // 页面全局属性
-      return {
-        aaaa:222222
-      }
+      return {}
     },
     created() {
       // 页面已生产
@@ -30,13 +29,19 @@
     },
     mounted() {
       // 页面已渲染完成
-      console.log(this.$children,'我是父组件');
+      console.log('我是子组件');
     },
     destroyed() {
       // 页面已退出
     },
     methods: {
       // 自定义函数
+      testMethod(){
+        console.log('子组件方法');
+      },
+      changeParent(){
+        this.$emit('update:bbc',3333)
+      }
     }
   }
 </script>
