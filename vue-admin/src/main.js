787fa96611831cc8a97ef8e60eb812b7132edadd
jerry <xuzhanhong@gialen.com>
2021-07-11 23:53:19 +0800

    修改配置

--- vue-admin/src/main.js
+++ vue-admin/src/main.js
@@ -1,9 +1,9 @@
-// import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
 import Vue from "vue";
 import router from "./router";
 import App from "./App.vue"
 // import store from "./store";
-// import ElementUI from 'element-ui';
+import ElementUI from 'element-ui';
 import axios from 'axios'
 import 'babel-polyfill'
 import Es6Promise from 'es6-promise'
